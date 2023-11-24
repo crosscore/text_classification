7d638bb5026f7c70deb71d63e1241bd37aa2b196
sakahara <sakahara.y-st@nhk.or.jp>
2023-11-24 10:50:48 +0900

    modified luke_ja_lite_v1.py

--- models/luke_japanese_lite/trainer/luke_ja_lite_v1.py
+++ models/luke_japanese_lite/trainer/luke_ja_lite_v1.py
@@ -2,40 +2,30 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from transformers import MLukeTokenizer, LukeForSequenceClassification, Trainer, TrainingArguments
 from datasets import Dataset
 import re
 import time
 
-def clean_text_for_bert(text):
-    # 連続するスペース、タブ、改行をスペースに置換
-    text = re.sub(r'\s+', ' ', text)
+def clean_text(text):
     text = text.strip()
     return text
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
-    # logitsがタプルの場合、その中身を確認
-    if isinstance(logits, tuple):
-        print("Logits is a tuple, contents:", logits)
-        # 予測スコアが含まれている部分を選択
-        logits = logits[0]
-
-    print("Logits shape:", logits.shape)
-    try:
-        predictions = np.argmax(logits, axis=-1)
-    except Exception as e:
-        print("Error in argmax:", e)
-        print("Logits:", logits)
-        raise
-    return {'accuracy': accuracy_score(labels, predictions)}
+    predictions = np.argmax(logits, axis=-1)
+    accuracy = accuracy_score(labels, predictions)
+    precision = precision_score(labels, predictions, average='weighted')
+    recall = recall_score(labels, predictions, average='weighted')
+    f1 = f1_score(labels, predictions, average='weighted')
+    return {'accuracy': accuracy, 'precision': precision, 'recall': recall, 'f1': f1}
 
 start = time.time()
-df = pd.read_csv('../../../data/scraping_data/csv/yahoo_news/concat/yahoo_news_concat_1123_v3.csv')
+df = pd.read_csv('../../../data/scraping_data/csv/yahoo_news/concat/yahoo_news_concat_1124_v3.csv')
 
 df['text'] = df['title'] + '。' + df['content']
-#df['text'] = df['text'].apply(clean_text_for_bert)
+df['text'] = df['text'].apply(clean_text)
 print(df)
 print(df['text'])
 
@@ -73,7 +63,7 @@ model = LukeForSequenceClassification.from_pretrained(PRE_TRAINED, num_labels=le
 # 訓練設定
 training_args = TrainingArguments(
     output_dir='./result',
-    num_train_epochs=5,
+    num_train_epochs=3,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
     evaluation_strategy="epoch",

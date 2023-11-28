56b7d8e9d3d18a25e3c96d5963d508b6b0116251
sakahara <sakahara.y-st@nhk.or.jp>
2023-11-28 14:49:12 +0900

    modified add_category_by_scraping.py

--- data/experimental_data/scripts/add_category_by_scraping.py
+++ data/experimental_data/scripts/add_category_by_scraping.py
@@ -19,40 +19,87 @@ category_dict = {
     'local': '地域',
 }
 
-def get_category_from_archive(url, max_retries=6, wait_seconds=10):
+def sanitize_filename(filename):
+    return re.sub(r'[\\/*?:"<>|]', '_', filename)
+
+def get_category_from_archive(url, max_retries=5, wait_seconds=12, max_wait_seconds=60):
     retries = 0
-    time.sleep(1.5)
+    time.sleep(6)
     while retries < max_retries:
         try:
             archive_url = f'https://web.archive.org/web/{url}'
-            response = requests.get(archive_url, timeout=20)
-            soup = BeautifulSoup(response.content, 'html.parser')
-            scripts = soup.find_all('script')
-            for script in scripts:
-                if 'currentCategory' in script.text:
-                    match = re.search(r'"currentCategory":"(\w+)"', script.text)
-                    if match:
-                        category_key = match.group(1)
-                        if category_key in category_dict:
-                            print(f'category: {category_key}')
-                            # "headline" キーの値を取得
-                            headline_match = re.search(r'"headline":"([^"]+)"', script.text)
-                            if headline_match:
-                                title = headline_match.group(1)
-                                print(f'title: {title}')
+            response = requests.get(archive_url, timeout=18)
+            response.raise_for_status() #HTTPエラーをチェック
+            if response.status_code == 200:
+                print("Request successful. Status code: 200")
+
+                # Save the HTML file only if status code is 200
+                html_dir = '../html/archive_files/'
+                os.makedirs(html_dir, exist_ok=True)
+                file_name = sanitize_filename(url.split("/")[-1]) + '.html'
+                with open(f'{html_dir}{url.split("/")[-1]}.html', 'w', encoding='utf-8') as f:
+                    f.write(response.text)
+                print(f'Saved HTML file: {file_name}')
+                
+                # HTMLの内容を解析してカテゴリを見つける
+                soup = BeautifulSoup(response.content, 'html.parser')
+                scripts = soup.find_all('script')
+                for script in scripts:
+                    if 'currentCategory' in script.text:
+                        print('Discover currentCategory.')
+                        match = re.search(r'"currentCategory":"(\w+)"', script.text)
+                        if match:
+                            print(f'match.group(1): {match.group(1)}')
+                            category_key = match.group(1)
+                            if category_key in category_dict:
+                                print(f'category_key: {category_key}')
+                                headline_match = re.search(r'"headline":"([^"]+)"', script.text)
+                                if headline_match:
+                                    title = headline_match.group(1)
+                                    print(f'headline_match.group(1): {title}')
+                                else:
+                                    title = 'None'
+                                return category_dict[category_key]
                             else:
-                                title = 'タイトル不明'
-                            return category_dict[category_key]
-                        else:
-                            print(f"category_dictに存在しないkey({category_key})です。")
-                            return None
-            return None  # スクリプト内でカテゴリが見つからなかった場合
-        except requests.RequestException as e:
-            print(f"Error processing URL {url}: {e}")
+                                print(f"category_dictに存在しないkey({category_key})です。")
+                                return None
+                else:
+                    print(f"Request failed with status code: {response.status_code}")
+                    retries += 1
+                    time.sleep(wait_seconds)
+                    wait_seconds = min(wait_seconds * 2, max_wait_seconds)  # 待機時間を指数的に増やすが最大値は超えない
+                    continue  # Retry the request
+
+        except requests.exceptions.Timeout as e:
+            print(f"Timeout error with URL {url}: {e}")
             retries += 1
             print(f"Retrying... ({retries}/{max_retries})")
             time.sleep(wait_seconds)
+        except requests.exceptions.ConnectionError as e:
+            print(f"Connection error with URL {url}: {e}")
+            retries += 1
+            print(f"Retrying... ({retries}/{max_retries})")
+            time.sleep(wait_seconds)
+        except requests.exceptions.HTTPError as e:
+            # 404エラーを特定して処理
+            if e.response.status_code == 404:
+                print(f"404 Not Found error for URL {url}: {e}")
+                return "not_found"
+            else:
+                print(f"HTTP error with URL {url}: {e}")
+                retries += 1
+                print(f"Retrying... ({retries}/{max_retries})")
+                time.sleep(wait_seconds)
+                continue
+        except requests.exceptions.TooManyRedirects as e:
+            print(f"Too many redirects for URL {url}: {e}")
+            return "too_many_redirects"
+        except requests.RequestException as e:
+            print(f"Error processing URL {url}: {e}")
+            return "request_exception"
+    
     return "retry_limit_exceeded"  # 最大リトライ回数を超えた場合
+
 
 def listen_for_exit_command():
     global exit_command_issued
@@ -72,6 +119,7 @@ exit_listener.start()
 df = pd.read_csv('../csv/original/device_original.csv', dtype={'user': str})
 # 'category' 列が存在しない場合は、空の列を作成
 if 'category' not in df.columns:
+    print("Adding 'None' to the category column")
     df['category'] = None
 print(df)
 
@@ -86,12 +134,16 @@ for index, row in df.iterrows():
     # 既にカテゴリが割り当てられている行はスキップ
     if pd.notna(row['category']):
         continue
-    category = get_category_from_archive(row['url'])
-    if category == "retry_limit_exceeded":
-        retry_limit_exceeded = True
-        print("Retry limit exceeded. Saving partial data...")
-        break
-    df.at[index, 'category'] = category
+    category = None
+    while category is None:
+        category = get_category_from_archive(row['url'])
+        if category == "retry_limit_exceeded" or category == "request_exception":
+            print("Retry limit exceeded. | RequestException occured. Saving partial data...")
+            retry_limit_exceeded = True
+            break
+    if category is not None:
+        df.at[index, 'category'] = category
+        print(f"Row {index+1}/{len(df)}: Added category '{category}'")
 
 if not exit_command_detected: #Enterが押された場合completeの保存はスキップするため
     if retry_limit_exceeded:
--- data/scraping_data/scripts/concat_only_unique_urls.py
+++ data/scraping_data/scripts/concat_only_unique_urls.py
@@ -1,46 +1,83 @@
-#csv_concat_and_to_only_unique_urls.py
 import pandas as pd
 import os
 import datetime
 import re
 import time
 
-# 今日の日付を取得し、指定されたフォーマットに変換
+# 今日の日付と前日の日付を取得
 today_date = datetime.datetime.now().strftime('%Y%m%d')
+yesterday_date = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y%m%d')
+
 # ディレクトリ内のファイル名を取得
 files = os.listdir('../csv/yahoo_news/concat/')
+
 # ファイル名から日付とバージョンを抽出するための正規表現
-pattern = re.compile(rf'{today_date}_v(\d+)\.csv$')
-# 最新のバージョンを探す
-latest_version = 0
-latest_file = ''
+pattern_yesterday = re.compile(rf'{yesterday_date}_v(\d+)\.csv$')
+pattern_today = re.compile(rf'{today_date}_v(\d+)\.csv$')
+
+# 前日の最新のバージョンを探す
+latest_version_yesterday = 0
+latest_file_yesterday = ''
+latest_version_today = 0
 for file in files:
-    match = pattern.search(file)
-    if match:
-        version = int(match.group(1))
-        if version > latest_version:
-            latest_version = version
-            latest_file = file
-# バージョンが見つからない場合は初期バージョンを設定
-if latest_version == 0:
-    latest_file = f'yahoo_news_concat_{today_date}_v1.csv'
-# 新しいバージョン番号を設定
-next_version = latest_version + 1
-input_old = os.path.join('../csv/yahoo_news/concat/', latest_file)
-input_new = f'../csv/yahoo_news/daily/yahoo_news_articles_{today_date}_v{next_version}.csv'
-output_file = f'../csv/yahoo_news/concat/yahoo_news_concat_{today_date}_v{next_version}.csv'
+    match_yesterday = pattern_yesterday.search(file)
+    match_today = pattern_today.search(file)
+    if match_yesterday:
+        version = int(match_yesterday.group(1))
+        if version > latest_version_yesterday:
+            latest_version_yesterday = version
+            latest_file_yesterday = file
+    elif match_today:
+        version = int(match_today.group(1))
+        if version > latest_version_today:
+            latest_version_today = version
+
+# 本日のファイルが存在するかチェックし、バージョンを設定
+if latest_version_today > 0:
+    version = latest_version_today + 1
+else:
+    version = 1
+
+# 本日のファイルが存在するかチェックし、バージョンを設定
+if latest_version_today > 0:
+    version = latest_version_today + 1
+    # 本日の2度目以降の実行の場合のファイル名設定
+    input_new = f'../csv/yahoo_news/daily/yahoo_news_articles_{today_date}_v{version}.csv'
+    output_file = f'../csv/yahoo_news/concat/yahoo_news_concat_{today_date}_v{version}.csv'
+    # 本日の2度目以降の実行の場合の前日のファイル名設定
+    input_old = f'../csv/yahoo_news/concat/yahoo_news_concat_{today_date}_v{latest_version_today}.csv'
+else:
+    # 本日初めての実行の場合のファイル名設定
+    input_new = f'../csv/yahoo_news/daily/yahoo_news_articles_{today_date}_v1.csv'
+    output_file = f'../csv/yahoo_news/concat/yahoo_news_concat_{today_date}_v1.csv'
+    # 本日初めての実行の場合の前日のファイル名設定
+    input_old = os.path.join('../csv/yahoo_news/concat/', latest_file_yesterday) if latest_file_yesterday else None
+
+
 print(f'input_old: {input_old}')
 print(f'input_new: {input_new}')
 print(f'output_file: {output_file}')
 os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-df_original = pd.read_csv(input_old)
+# 前日のファイルが存在すれば読み込む、存在しなければ空のDataFrameを作成
+if input_old and os.path.exists(input_old):
+    df_original = pd.read_csv(input_old)
+else:
+    df_original = pd.DataFrame()
+
 print(f'before: {df_original["url"].nunique()}')
 
+# 本日のデータの読み込み
 df_new = pd.read_csv(input_new)
+
+# データの結合
 df_concat = pd.concat([df_original, df_new])
-df_concat.drop_duplicates(subset=['url'], inplace=True) # 'url'列の値が重複する場合削除
+
+# 'url'列の値が重複する場合削除
+df_concat.drop_duplicates(subset=['url'], inplace=True)
+
 print(f"after : {df_concat['url'].nunique()}")
 print(df_concat['category'].value_counts())
 
+# 結合したデータを保存
 df_concat.to_csv(output_file, index=False)

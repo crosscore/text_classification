dbe28ac6beaf8537b742d8f5050711ee353ee911
yuu <crosscore.g@gmail.com>
2023-12-02 12:09:11 +0900

    modified scraping_data script

--- data/scraping_data/scripts/scraping_from_yahoo_news.py
+++ data/scraping_data/scripts/scraping_from_yahoo_news.py
@@ -1,4 +1,4 @@
-#scraping_from_yahoo_news_v2.py
+#scraping_from_yahoo_news.py
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -27,15 +27,17 @@ if os.path.exists(concat_dir) and os.listdir(concat_dir):
     output_file = f'../csv/yahoo_news/daily/yahoo_news_articles_{today_date}_v{next_version}.csv'
     if latest_file:
         latest_df = pd.read_csv(os.path.join(concat_dir, latest_file))
+        print(f'latest_df: {latest_df}')
         skip_urls = set(latest_df['url'].dropna().unique())
 else:
     print("First scraping today or directory does not exist or is empty.")
     output_file = f'../csv/yahoo_news/daily/yahoo_news_articles_{today_date}_v1.csv'
 
-print(output_file)
+print(f'output_file: {output_file}')
 os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-base_url = 'https://news.yahoo.co.jp/rss/categories/'
+rss_categories = 'https://news.yahoo.co.jp/rss/categories/'
+rss_topics = 'https://news.yahoo.co.jp/rss/topics/'
 categories = {
     '国内': 'domestic',
     '国際': 'world',
@@ -47,7 +49,14 @@ categories = {
     'ライフ': 'life',
     '地域': 'local'
 }
-urls = {category: f'{base_url}{categories[category]}.xml' for category in categories}
+category_urls = {category: f'{rss_categories}{categories[category]}.xml' for category in categories}
+topic_categories = {k: v for k, v in categories.items() if k != 'ライフ'}
+topic_urls = {category: f'{rss_topics}{topic_categories[category]}.xml' for category in topic_categories}
+print(category_urls)
+print(topic_urls)
+
+# 重複排除用セット
+all_urls_set = set()
 
 skipped_articles = 0
 new_articles_count = 0
@@ -56,12 +65,14 @@ RETRY_INTERVAL = 12 # seconds
 
 def scrape_news(category, url):
     global new_articles_count
+    global all_urls_set
     for i in range(MAX_RETRIES):
         try:
             print(f'scrape_news... {category}: {url}')
             response = requests.get(url, timeout=(12, 18))
-            print(response.status_code)
-            soup = BeautifulSoup(response.content, 'html.parser')
+            print(f'response.status_code: {response.status_code}')
+            # XML パーサーを使用するように変更
+            soup = BeautifulSoup(response.content, 'xml')
             if "指定されたURLは存在しませんでした。" in str(soup):
                 print(f"Error page detected, skipping remaining page for ({category}) url:{url}")
                 return []
@@ -69,10 +80,11 @@ def scrape_news(category, url):
             for item in soup.find_all("item"):
                 title = item.find("title").get_text(strip=True)
                 link_tag = item.find("link")
-                link = link_tag.next_sibling.strip() if link_tag else ""
-                if link in skip_urls:
+                link = link_tag.string.strip() if link_tag.string else ""
+                if link in skip_urls or link in all_urls_set:
                     print(f"Skipping {link} as it's already scraped.")
                     continue
+                all_urls_set.add(link)
                 new_articles_count += 1
                 print(f"{category}: {title}")
                 print(f"{link}")
@@ -107,14 +119,23 @@ def scrape_article_content(link,  MAX_RETRIES = 6):
 print('Start scraping.')
 start = time.time()
 all_articles = []
-for category, url in urls.items():
+
+for category, url in category_urls.items():
     print(f'{category}: {url}')
     articles = scrape_news(category, url)
-    if not articles:
-        break
-    print(f'Found {len(articles)} articles.')
-    all_articles.extend(articles)
+    if articles:
+        print(f'Found {len(articles)} articles.')
+        all_articles.extend(articles)
     time.sleep(1)
+
+for category, url in topic_urls.items():
+    print(f'{category}: {url}')
+    articles = scrape_news(category, url)
+    if articles:
+        print(f'Found {len(articles)} articles.')
+        all_articles.extend(articles)
+    time.sleep(1)
+
 print(f'Complete. Found {len(all_articles)} articles.')
 
 all_articles_data = []
@@ -125,6 +146,7 @@ for article in all_articles:
     print(f'Found {len(content)} characters.')
     all_articles_data.append([category, title, link, content])
     time.sleep(0.9)
+
 df = pd.DataFrame(all_articles_data, columns=['category', 'title', 'url', 'content'])
 print(f"df['url'].duplicated().sum(): {df['url'].duplicated().sum()}")
 df.to_csv(output_file, index=False, encoding='utf-8')

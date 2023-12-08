43f28269833892660b211f49321acb06dd8b7e4f
yuu <crosscore.g@gmail.com>
2023-12-08 17:58:43 +0900

    update csv

--- data/experimental_data/scripts/utils/check_csv.py
+++ data/experimental_data/scripts/utils/check_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import glob
 
-csv_files = glob.glob('../../csv/complete/*.csv')
+csv_files = glob.glob('../../csv/original/*.csv')
 #csv_files = glob.glob('../csv/original/all_404_not_found_v3.csv')
 print(f'csv_files: {csv_files[0]}')
 df = pd.read_csv(csv_files[0], encoding='utf-8', dtype={'user': str})

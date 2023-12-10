96a89509916f2fd9d35b9f2f45c5d9ab294f3b3e
yuu <crosscore.g@gmail.com>
2023-12-10 17:07:41 +0900

    modified luke ja base script

--- data/scraping_data/scripts/concat_only_unique_urls.py
+++ data/scraping_data/scripts/concat_only_unique_urls.py
@@ -39,7 +39,8 @@ new_file = f'../csv/yahoo_news/daily/yahoo_news_articles_{today_date}_v{version}
 output_file = f'../csv/yahoo_news/concat/yahoo_news_concat_{today_date}_v{version}.csv'
 original_file = f'../csv/yahoo_news/concat/yahoo_news_concat_{today_date}_v{latest_version_today}.csv' if latest_version_today > 0 else os.path.join('../csv/yahoo_news/concat/', latest_file_yesterday) if latest_file_yesterday else None
 os.makedirs(os.path.dirname(output_file), exist_ok=True)
-print("====== exec concat_only_unique_urls.py ======")
+
+print("============ exec concat_only_unique_urls.py ============")
 print(f'input_old: {original_file}')
 print(f'input_new: {new_file}')
 print(f'output_file: {output_file}')
@@ -48,9 +49,9 @@ if original_file and os.path.exists(original_file):
     df_original = pd.read_csv(original_file)
 else:
     df_original = pd.DataFrame()
+print(f"df_original['category'].value_counts(dropna=False):\n{df_original['category'].value_counts(dropna=False)}")
 
-print(f"befor : {df_original['url'].nunique()}")
-
+before_num = df_original['url'].nunique()
 df_new = pd.read_csv(new_file)
 df_concat = pd.concat([df_original, df_new])
 
@@ -65,17 +66,13 @@ df_concat = df_concat[(df_concat['title'].apply(lambda x: isinstance(x, str))) &
 # カテゴリの処理
 df_concat['category'] = pd.Categorical(df_concat['category'], categories=category_list, ordered=True)
 df_concat.sort_values(by='category', inplace=True)
-
 df_concat.to_csv(output_file, index=False)
-print(f"after : {df_concat['url'].nunique()}")
 
 # データの確認
 #df = df_concat.copy()[~df_concat['url'].str.contains('/pickup/')]
 df = df_concat.copy()
 print('---------')
 print(f'df.isnull().sum():\n{df.isnull().sum()}')
-print('---------')
-print(f'df.info():\n{df.info()}')
 print('---------')
 print(f'df.describe():\n{df.describe()}')
 print('---------')
@@ -85,8 +82,11 @@ print(f"df['category'].value_counts(dropna=False):\n{df['category'].value_counts
 print('---------')
 for column in df.columns:
     print(f"df['{column}'].duplicated().sum(): {df[column].duplicated().sum()}")
-
+print('---------')
 #'../csv/yahoo_news/daily/'フォルダの.csvファイルを全削除
 for file in os.listdir('../csv/yahoo_news/daily/'):
     os.remove(os.path.join('../csv/yahoo_news/daily/', file))
     print(f"remove: {file}")
+print('---------')
+print(f"befor : {before_num}")
+print(f"after : {df_concat['url'].nunique()}")

692f94ed841e096979f2dd5c98edb5e001e5b547
yuu <crosscore.g@gmail.com>
2023-12-14 12:21:34 +0900

    added csv compression and decompression scripts

--- data/scraping_data/scripts/subprocess_script.py
+++ data/scraping_data/scripts/subprocess_script.py
@@ -1,10 +1,14 @@
 #subprocess_script.py
 import subprocess
 
+script_file0 = "./unzip_zip_file.py"
 script_file1 = "./scraping_from_yahoo_news.py"
 script_file2 = "./concat_only_unique_urls.py"
 script_file3 = "./remove_text_outliers.py"
+script_file4 = "./compress_csv_to_zip.py"
 
+subprocess.run(["python", script_file0])
 subprocess.run(["python", script_file1])
 subprocess.run(["python", script_file2])
 subprocess.run(["python", script_file3])
+subprocess.run(["python", script_file4])
---
+++ data/scraping_data/scripts/unzip_zip_file.py
@@ -0,0 +1,26 @@
+import os
+import zipfile
+
+directory = '../csv/yahoo_news/concat/'
+
+# List all files in the directory
+files = os.listdir(directory)
+
+# Find the first ZIP file
+zip_file = next((f for f in files if f.endswith('.zip')), None)
+
+if zip_file:
+    # Full path for the ZIP file
+    zip_path = os.path.join(directory, zip_file)
+
+    # Extracting the ZIP file
+    with zipfile.ZipFile(zip_path, 'r') as zipf:
+        zipf.extractall(directory)
+
+    # Remove the original ZIP file
+    os.remove(zip_path)
+
+    # Print complete messages
+    print(f'ZIP file {zip_file} extracted and removed successfully.')
+else:
+    print("No ZIP file found in the directory.")

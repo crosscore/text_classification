63ad99fbc571cb691da8eb0e3c67c886bd2dc07b
yuu <crosscore.g@gmail.com>
2023-12-21 07:14:54 +0000

    update test scripts

--- test/scripts/fugashi_test.py
+++ test/scripts/fugashi_test.py
@@ -1,32 +1,35 @@
-import spacy
 from fugashi import Tagger
+import MeCab
 
-def extract_nouns_adjs(text):
-    words = []
-    doc = nlp(text)
-    for token in doc:
-        if token.pos_ in ['NOUN', 'ADJ']:
-            words.append(token.text)
-    return " ".join(words)
+# def extract_nouns_adjs(text):
+#     words = []
+#     doc = nlp(text)
+#     for token in doc:
+#         if token.pos_ in ['NOUN', 'ADJ']:
+#             words.append(token.text)
+#     return " ".join(words)
 
-nlp = spacy.load('ja_ginza_electra')
-sample_txt = '私が最近見た映画は、約束のネバーランドでした。スターバックス鬼滅の刃コラボ。スタバはインスタ映え。'
-doc = nlp(sample_txt)
+# nlp = spacy.load('ja_ginza_electra')
+# doc = nlp(sample_txt)
 
-print('------')
-print(doc)
+# print('------')
+# print(f'doc: {doc}')
+# print([token.text for token in doc if token.pos_ in ['NOUN','ADJ']])
+# print(f'extract_nouns_adjs(sample_txt):{extract_nouns_adjs(sample_txt)}')
+# print('------')
 
-print('------')
-print([token.text for token in doc if token.pos_ in ['NOUN','ADJ']])
+# for token in doc:
+#     print(token.text, token.pos_, token.tag_, token.dep_)
 
+sample_txt = 'スターバックスはスタバです。鬼滅の刃が上映中。すもももももももものうち。コロナ禍で大変です'
 
-print('------')
-print(extract_nouns_adjs(sample_txt))
-
-print('------')
-for token in doc:
-    print(token.text, token.pos_, token.tag_, token.dep_)
+# Specify the path of Neologd
+neologd_path = '/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd'
 
 print('------')
 tagger = Tagger()
-print(tagger.parse(sample_txt))
+print(f'tagger.parse(sample_txt): fugashi test\n{tagger.parse(sample_txt)}')
+
+print('------')
+mecab = MeCab.Tagger('-d' + neologd_path)
+print(f'tagger.parse(sample_txt): MeCab test\n{mecab.parse(sample_txt)}')
--- test/scripts/test_probability.py
+++ test/scripts/test_probability.py
@@ -1,12 +1,50 @@
-# Define the probabilities of A and B
-p_A = 0.9  # P(A)
-p_B = 0.8  # P(B)
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-# Calculate P(A∩B) for independent events
-p_A_and_B = p_A * p_B
+def calculate_union_probability(p_A, p_B):
+    """
+    Calculate the probability of the union of two independent events A and B.
 
-# Calculate P(A∪B) using the formula
-p_A_or_B = p_A + p_B - p_A_and_B
+    Parameters:
+    p_A (float): Probability of event A
+    p_B (float): Probability of event B
 
-# Output the result
-print(f"The probability of P(A∪B) when A and B are independent is: {p_A_or_B}")
+    Returns:
+    float: Probability of the union of A and B
+    """
+    p_A_and_B = p_A * p_B
+    p_A_or_B = p_A + p_B - p_A_and_B
+    return p_A_or_B
+
+# Generate a range of probabilities from 0 to 1
+probabilities = np.linspace(0, 1, 100)
+
+# Prepare data for graphing
+data = []
+for p_A in probabilities:
+    for p_B in probabilities:
+        p_A_or_B = calculate_union_probability(p_A, p_B)
+        data.append([p_A, p_B, p_A_or_B])
+
+# Convert the data into a Pandas DataFrame
+data_df = pd.DataFrame(data, columns=['p_A', 'p_B', 'p_A_or_B'])
+
+# Pivot the DataFrame for the heatmap
+pivot_df = data_df.pivot(index='p_B', columns='p_A', values='p_A_or_B')
+
+# Define labels for the x and y axes with 2 decimal places
+label_interval = 5  # Adjust the interval as needed
+xtick_labels = [f"{x:.2f}" if i % label_interval == 0 else "" for i, x in enumerate(pivot_df.columns)]
+ytick_labels = [f"{y:.2f}" if i % label_interval == 0 else "" for i, y in enumerate(pivot_df.index)]
+
+# Plot the data
+plt.figure(figsize=(10, 6))
+sns.heatmap(pivot_df, annot=False, cmap='coolwarm', xticklabels=xtick_labels, yticklabels=ytick_labels)
+plt.title("Probability of P(A∪B) for Various Values of P(A) and P(B)")
+plt.xlabel("Probability of B (p_B)")
+plt.ylabel("Probability of A (p_A)")
+plt.xticks(rotation=90)  # Rotate x-axis labels for better readability
+plt.yticks(rotation=0)   # Set y-axis labels straight
+plt.savefig("probability_union.png")

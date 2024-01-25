cf437a50d9ca5cfbcc52bf93fa7d431640934bc2
yuu <crosscore.g@gmail.com>
2024-01-25 09:49:46 +0900

    update zip

--- models/luke_japanese/trainer/luke_ja_lite_restart.py
+++ models/luke_japanese/trainer/luke_ja_lite_restart.py
@@ -111,7 +111,7 @@ trainer = Trainer(
 )
 
 # Resume training from existing checkpoint
-checkpoint_path = "./result/checkpoint-3344/"
+checkpoint_path = "./result/checkpoint-5016/"
 trainer.train(resume_from_checkpoint=checkpoint_path)
 
 output_dir = '../versions/v101/'
